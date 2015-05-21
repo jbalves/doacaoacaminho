df569ea08c76fa10434230cc94c251875886adfc
Jeferson Barros Alves <jbalves@MacBook-Pro-de-Jeferson.local>
2015-05-21 09:06:35 -0400

    Initial commit, create project

---
+++ DoacaoACaminho/DoacaoACaminho/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DoacaoACaminho
+//
+//  Created by Jeferson Barros Alves on 21/05/15.
+//  Copyright (c) 2015 Jeferson Barros Alves. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ DoacaoACaminho/DoacaoACaminho/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DoacaoACaminho
+//
+//  Created by Jeferson Barros Alves on 21/05/15.
+//  Copyright (c) 2015 Jeferson Barros Alves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
