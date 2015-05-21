fb926e00a7b0f0520ba93999111a8eb13327f1ce
Rodrigo DAngelo SIlva Machado <rodrigodangelo95@icloud.com>
2015-05-21 09:40:28 -0400

    Criando as classes

---
+++ DoacaoACaminho/DoacaoACaminho/MapaViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapaViewController.h
+//  DoacaoACaminho
+//
+//  Created by Rodrigo DAngelo Silva Machado on 5/21/15.
+//  Copyright (c) 2015 Jeferson Barros Alves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapaViewController : UIViewController
+
+@end
---
+++ DoacaoACaminho/DoacaoACaminho/MapaViewController.m
@@ -0,0 +1,26 @@
+//
+//  MapaViewController.m
+//  DoacaoACaminho
+//
+//  Created by Rodrigo DAngelo Silva Machado on 5/21/15.
+//  Copyright (c) 2015 Jeferson Barros Alves. All rights reserved.
+//
+
+#import "MapaViewController.h"
+
+@interface MapaViewController ()
+
+@end
+
+@implementation MapaViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    }
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+@end
