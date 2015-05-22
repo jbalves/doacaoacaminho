3ce7cf2ee3f9242b22aab6c2f36583b30ccb2cd0
Carlos Amadheus Souza Araujo <K_Amadheus@Carlos-Amadheus-Mac.local>
2015-05-22 08:56:44 -0400

    Creating Class

---
+++ DoacaoACaminho/DoacaoACaminho/DACInstituicoesViewController.h
@@ -0,0 +1,13 @@
+//
+//  DACInstituicoesViewController.h
+//  DoacaoACaminho
+//
+//  Created by Carlos Amadheus Souza Araujo on 22/05/15.
+//  Copyright (c) 2015 Jeferson Barros Alves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DACInstituicoesViewController : UIViewController
+
+@end
---
+++ DoacaoACaminho/DoacaoACaminho/DACInstituicoesViewController.m
@@ -0,0 +1,13 @@
+//
+//  DACInstituicoesViewController.m
+//  DoacaoACaminho
+//
+//  Created by Carlos Amadheus Souza Araujo on 22/05/15.
+//  Copyright (c) 2015 Jeferson Barros Alves. All rights reserved.
+//
+
+#import "DACInstituicoesViewController.h"
+
+@implementation DACInstituicoesViewController
+
+@end
