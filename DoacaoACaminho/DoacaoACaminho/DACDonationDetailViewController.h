eddeccd70d3cb51d2037dceae2d83d401893f6f8
Patrick Magalhães de Lima <PatrickLima@MacBook-Patrick.local>
2015-07-13 14:39:30 -0400

    adição dos elementos da tela de detalhes da doação na storyboard e criação dos outlets na view controller

---
+++ DoacaoACaminho/DoacaoACaminho/DACDonationDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DACDonationDetailViewController.h
+//  DoacaoACaminho
+//
+//  Created by Patrick Magalhães de Lima on 13/07/15.
+//  Copyright (c) 2015 Jeferson Barros Alves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DACDonationDetailViewController : UIViewController
+
+@end
