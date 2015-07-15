02f752da104f44234703cd7f671f7e02ed1fc8b1
Patrick Magalhães de Lima <PatrickLima@MacBook-Patrick.local>
2015-07-15 16:12:34 -0400

    aplicando design (básico)

--- DoacaoACaminho/DoacaoACaminho/AppDelegate.m
+++ DoacaoACaminho/DoacaoACaminho/AppDelegate.m
@@ -21,6 +21,10 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    
+    UIColor *mainColor = [UIColor colorWithRed:19.0/255 green:71.0/255 blue:121.0/255 alpha:0.9];
+    [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:19.0/255 green:71.0/255 blue:121.0/255 alpha:0.9]];
+    [[UITabBar appearance] setBarTintColor:mainColor];
     // Override point for customization after application launch.
     [Parse setApplicationId:@"hvnIoSFbTTNV2dlp2hNroqX6zFe72eiREyheqrYi"
                   clientKey:@"idoZRMFGCwOQT5wuL3tb9d9HWt8T5XpvQLSIoSTd"];
